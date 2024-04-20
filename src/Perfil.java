8a64626849a4752a75000afcf8746e9803e3bd3c
Ana-Neves <ana.carolinemms22@gmail.com>
2024-04-20 15:25:30 -0300

    criando as primeiras classes

---
+++ src/Perfil.java
@@ -0,0 +1,10 @@
+public class Perfil{
+
+    public static void main(String[] args) {
+        System.out.println("Olá, sou Ana");
+        System.out.println("Tudo bem?");
+        System.out.println(10 + 5);
+        System.out.println(10 - 5);
+    }
+
+}
